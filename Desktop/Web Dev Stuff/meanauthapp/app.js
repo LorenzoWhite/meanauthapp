8fe374d5a9ada7eafae17d1a8b36f7a5d48c1130
Lorenzo White <lorenzowhite@Lorenzos-MacBook-Pro-2.local>
2017-11-15 17:52:22 -0500

    1st video completed build

---
+++ Desktop/Web Dev Stuff/meanauthapp/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database')
+
+//database middleware
+mongoose.connect(config.database);
+
+//testing connection to database
+mongoose.connection.on('connected', () => {
+  console.log('connected to database '+ config.database)
+})
+
+mongoose.connection.on('error', (err) => {
+  console.log('database error '+ err)
+})
+
+const app = express();
+
+const users = require('./routes/users')
+//port number
+const port = 3000
+
+//cors middleware
+app.use(cors());
+
+//set STATIC folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Body Parser middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+
+app.get('/', (req, res) =>{
+  res.send('invalid endpoint')
+});
+
+app.listen(port, () => {
+  console.log('server started on port '+port);
+})
---
+++ Desktop/Web Dev Stuff/meanauthapp/routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+//register
+router.get('/register', (req, res, next) => {
+  res.send('REGISTER')
+});
+
+//authenticate
+router.get('/authenticate', (req, res, next) => {
+  res.send('AUTHENTICATE')
+});
+
+//profile
+router.get('/profile', (req, res, next) => {
+  res.send('profile')
+});
+
+//validate
+router.get('/validate', (req, res, next) => {
+  res.send('validate')
+});
+
+
+
+module.exports = router;
